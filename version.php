811355778832492c744b20da9472b6f2a66c5694
wwwelte <werner.welte@gmx.de>
2016-01-19 18:18:34 +0100

    Commit No Uno

---
+++ version.php
@@ -0,0 +1,7 @@
+<?php
+
+$plugin->version   = 2015112400;       // The current module version (Date: YYYYMMDDXX)
+$plugin->requires  = 2010090500;      // Requires this Moodle version
+$plugin->component = 'block_lradmin';    // Full name of the plugin (used for diagnostics)
+$plugin->cron      = 0;
+
