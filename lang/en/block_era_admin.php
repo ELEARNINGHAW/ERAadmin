6e1323841a42499b7035df29ea3e384cfdc364f8
wwwelte <werner.welte@gmx.de>
2016-01-20 20:10:34 +0100

    link zu er_anfrage.NEW

---
+++ lang/en/block_era_admin.php
@@ -0,0 +1,8 @@
+<?php
+$string['pluginname'] = 'EMIL-Raum Anfrage Admin';
+$string['blockstring'] = 'Sichtbare Kurskategorien';
+$string['era_admin:addinstance'] = 'Block ER ANFRAGE ADMIN ';
+$string['era_admin:myaddinstance'] = 'Block ER ANFRAGE ADMIN in MY';
+
+
+$string['blockstring'] = ''; 
