6da68e2aec22f5eecff7d67a768d42f9a49d4cf8
wwwelte <werner.welte@gmx.de>
2016-01-21 15:09:39 +0100

    Merge origin/master
    Conflicts:
    	db/access.php

--- lang/en/block_lradmin.php
+++
@@ -1,8 +0,0 @@
-<?php
-$string['pluginname'] = 'Lernraum Anfrage Admin';
-$string['blockstring'] = 'Sichtbare Kurskategorien';
-$string['lradmin:addinstance'] = 'Block LR ANFRAGE ADMIN ';
-$string['lradmin:myaddinstance'] = 'Block LR ANFRAGE ADMIN in MY';
-
-
-$string['blockstring'] = ''; 
